ffb931d7cbfaf1ae0fd098f484d5f0038e5c2a0a
Simon Jagoe <sjagoe@enthought.com>
2013-05-09 17:19:42 +0100

    Updated project.clj

--- project.clj
+++ project.clj
@@ -1,6 +1,7 @@
 (defproject octobuilder "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "octobuilder is a bridge between Jenkins and GitHub to
+  facilitate automatic building of pull requests"
+  :url "https://github.com/sjagoe/octobuilder"
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [compojure "1.1.5"]]
   :plugins [[lein-ring "0.8.3"]]
