e825efbdc7dd6bf65a2a7fcc686263778485dd75
Simon Jagoe <sjagoe@enthought.com>
2013-05-12 13:23:03 +0100

    Added simple views for testing

--- src/octobuilder/handler.clj
+++ src/octobuilder/handler.clj
@@ -5,12 +5,14 @@
         [ring.middleware.keyword-params :only [wrap-keyword-params]]
         [clj-oauth2.ring :only [wrap-oauth2]])
   (:require [octobuilder.oauth2 :as octo-auth]
+            [octobuilder.views :as views]
             [compojure.handler :as handler]
             [compojure.route :as route]))
 
 
 (defroutes app-routes
-  (GET "/" [] "Hello World")
+  (GET "/" [] (views/base-view))
+  (GET "/projects" [views/project-list])
   (route/resources "/")
   (route/not-found "Not Found"))
 
---
+++ src/octobuilder/views.clj
@@ -0,0 +1,8 @@
+(ns octobuilder.views)
+
+(def base-view []
+  "Hello World")
+
+
+(def project-list []
+  "List of projects")
