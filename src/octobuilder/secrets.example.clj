3549a9cc8e134bc623098190303e32a1c9055654
Simon Jagoe <sjagoe@enthought.com>
2013-05-16 21:56:52 +0100

    Added example secrets file

---
+++ src/octobuilder/secrets.example.clj
@@ -0,0 +1,13 @@
+(ns octobuilder.secrets)
+
+
+(def client-id "00000000000000000000")
+
+
+(def client-secret "0000000000000000000000000000000000000000")
+
+
+(def projects [{:owner "company"
+                :project "project"
+                :user "username"
+                :token "user-token"}])
