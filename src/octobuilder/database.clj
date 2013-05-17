3fb63aa2119aee5d922da5093e92945b7b7bca0b
Simon Jagoe <sjagoe@enthought.com>
2013-05-17 16:38:31 +0100

    Updated schema to support jenkins jobs; updated bootstrap

--- src/octobuilder/database.clj
+++ src/octobuilder/database.clj
@@ -13,84 +13,85 @@
 (def conn (d/connect database-uri))
 
 
+(defn make-schema-part
+  ([ident type cardinality doc]
+     (make-schema-part ident type cardinality doc false))
+  ([ident type cardinality doc fulltext]
+     {:db/id (d/tempid :db.part/db)
+      :db/ident name
+      :db/valueType type
+      :db/cardinality cardinality
+      :db/fulltext fulltext
+      :db/doc doc
+      :db.install/_attribute :db.part/db}))
+
+
 (def schema
-  [;; projects
+  [;; Jenkins job
+   (make-schema-part :jenkins.job/name
+                     :db.type/string
+                     :db.cardinality/one
+                     "The name of a Job in Jenkins")
+   (make-schema-part :jenkins.job/location
+                     :db.type/string
+                     :db.cardinality/one
+                     "The URL of the job")
+   (make-schema-part :jenkins.job/user
+                     :db.type/ref
+                     :db.cardinality/one
+                     "The user used to authenticate to Jenkins to trigger builds of the job")
+
+   ;; projects
    ; owner name; string
-   {:db/id (d/tempid :db.part/db)
-    :db/ident :project/owner
-    :db/valueType :db.type/string
-    :db/cardinality :db.cardinality/one
-    :db/fulltext false
-    :db/doc "The name of the project's owner"
-    :db.install/_attribute :db.part/db}
+   (make-schema-part :project/owner
+                     :db.type/string
+                     :db.cardinality/one
+                     "The name of the project's owner")
    ; project name; string
-   {:db/id (d/tempid :db.part/db)
-    :db/ident :project/name
-    :db/valueType :db.type/string
-    :db/cardinality :db.cardinality/one
-    :db/fulltext false
-    :db/doc "The name of the project repository"
-    :db.install/_attribute :db.part/db}
+   (make-schema-part :project/name
+                     :db.type/string
+                     :db.cardinality/one
+                     "The name of the project repository")
    ; project user; ref
-   {:db/id (d/tempid :db.part/db)
-    :db/ident :project/user
-    :db/valueType :db.type/ref
-    :db/cardinality :db.cardinality/one
-    :db/fulltext false
-    :db/doc "Reference to the user for logging in to github"
-    :db.install/_attribute :db.part/db}
+   (make-schema-part :project/user
+                     :db.type/ref
+                     :db.cardinality/one
+                     "Reference to the user for logging in to github")
+   (make-schema-part :project/jenkins-job
+                     :db.type/ref
+                     :db.cardinality/one
+                     "Jenkins job used to build pull requests of this project")
 
    ;; Users
-   {:db/id (d/tempid :db.part/db)
-    :db/ident :github/access-token
-    :db/valueType :db.type/string
-    :db/cardinality :db.cardinality/one
-    :db/fulltext false
-    :db/doc "The user's access token"
-    :db.install/_attribute :db.part/db}
-
-   {:db/id (d/tempid :db.part/db)
-    :db/ident :user/username
-    :db/valueType :db.type/string
-    :db/cardinality :db.cardinality/one
-    :db/fulltext false
-    :db/doc "The user's login name"
-    :db.install/_attribute :db.part/db}
-
-   {:db/id (d/tempid :db.part/db)
-    :db/ident :jenkins/password
-    :db/valueType :db.type/string
-    :db/cardinality :db.cardinality/one
-    :db/fulltext false
-    :db/doc "The user's password"
-    :db.install/_attribute :db.part/db}
-
-   {:db/id (d/tempid :db.part/db)
-    :db/ident :user/service
-    :db/valueType :db.type/ref
-    :db/cardinality :db.cardinality/one
-    :db/fulltext false
-    :db/doc "The service for which this user is used"
-    :db.install/_attribute :db.part/db}
+   (make-schema-part :github/access-token
+                     :db.type/string
+                     :db.cardinality/one
+                     "The user's access token")
+   (make-schema-part :user/username
+                     :db.type/string
+                     :db.cardinality/one
+                     "The user's login name")
+   (make-schema-part :jenkins/password
+                     :db.type/string
+                     :db.cardinality/one
+                     "The user's password")
+   (make-schema-part :user/service
+                     :db.type/ref
+                     :db.cardinality/one
+                     "The service for which this user is used")
 
    ;; services
-   {:db/id (d/tempid :db.part/db)
-    :db/ident :service/name
-    :db/valueType :db.type/string
-    :db/cardinality :db.cardinality/one
-    :db/fulltext false
-    :db/doc "The name of this service"
-    :db.install/_attribute :db.part/db}
+   (make-schema-part :service/name
+                     :db.type/string
+                     :db.cardinality/one
+                     "The name of this service")
 
    ;;; pull request components
    ;; github.general/id; long
-   {:db/id (d/tempid :db.part/db)
-    :db/ident :github.general/id
-    :db/valueType :db.type/long
-    :db/cardinality :db.cardinality/one
-    :db/fulltext false
-    :db/doc "The internal GitHub object ID"
-    :db.install/_attribute :db.part/db}
+   (make-schema-part :github.general/id
+                     :db.type/long
+                     :db.cardinality/one
+                     "The internal GitHub object ID")
 
    ;; user types
    [:db/add (d/tempid :db.part/user) :db/ident :github.usertype/user]
@@ -99,166 +100,133 @@
    ;; github.user
    ; github.general/id
    ; type; [organization user]
-   {:db/id (d/tempid :db.part/db)
-    :db/ident :github.user/type
-    :db/valueType :db.type/ref ; :github.usertype/*
-    :db/cardinality :db.cardinality/one
-    :db/fulltext false
-    :db/doc "The type of the user"
-    :db.install/_attribute :db.part/db}
+   (make-schema-part :github.user/type
+                     :db.type/ref       ; :github.usertype/*
+                     :db.cardinality/one
+                     "The type of the user")
    ; login; string
-   {:db/id (d/tempid :db.part/db)
-    :db/ident :github.user/login
-    :db/valueType :db.type/string
-    :db/cardinality :db.cardinality/one
-    :db/fulltext false
-    :db/doc "The login name of the user"
-    :db.install/_attribute :db.part/db}
+   (make-schema-part :github.user/login
+                     :db.type/string
+                     :db.cardinality/one
+                     "The login name of the user")
 
    ;; github.repository
    ; github.general/id
    ; name; string
-   {:db/id (d/tempid :db.part/db)
-    :db/ident :github.repository/name
-    :db/valueType :db.type/string
-    :db/cardinality :db.cardinality/one
-    :db/fulltext false
-    :db/doc "The name of the repository"
-    :db.install/_attribute :db.part/db}
+   (make-schema-part :github.repository/name
+                     :db.type/string
+                     :db.cardinality/one
+                     "The name of the repository")
    ; full-name; string
-   {:db/id (d/tempid :db.part/db)
-    :db/ident :github.repository/full-name
-    :db/valueType :db.type/string
-    :db/cardinality :db.cardinality/one
-    :db/fulltext false
-    :db/doc "The full name of the repository"
-    :db.install/_attribute :db.part/db}
+   (make-schema-part :github.repository/full-name
+                     :db.type/string
+                     :db.cardinality/one
+                     "The full name of the repository")
    ; owner
-   {:db/id (d/tempid :db.part/db)
-    :db/ident :github.repository/owner
-    :db/valueType :db.type/ref ; github.user
-    :db/cardinality :db.cardinality/one
-    :db/fulltext false
-    :db/doc "The owner of the repository"
-    :db.install/_attribute :db.part/db}
+   (make-schema-part :github.repository/owner
+                     :db.type/ref       ; github.user
+                     :db.cardinality/one
+                     "The owner of the repository")
 
    ;; github.head
    ; label; string
-   {:db/id (d/tempid :db.part/db)
-    :db/ident :github.head/label
-    :db/valueType :db.type/string
-    :db/cardinality :db.cardinality/one
-    :db/fulltext false
-    :db/doc "The label of the HEAD"
-    :db.install/_attribute :db.part/db}
+   (make-schema-part :github.head/label
+                     :db.type/string
+                     :db.cardinality/one
+                     "The label of the HEAD")
    ; ref; string
-   {:db/id (d/tempid :db.part/db)
-    :db/ident :github.head/ref
-    :db/valueType :db.type/string
-    :db/cardinality :db.cardinality/one
-    :db/fulltext false
-    :db/doc "The ref pointing to the HEAD"
-    :db.install/_attribute :db.part/db}
+   (make-schema-part :github.head/ref
+                     :db.type/string
+                     :db.cardinality/one
+                     "The ref pointing to the HEAD")
    ; sha; string
-   {:db/id (d/tempid :db.part/db)
-    :db/ident :github.head/sha
-    :db/valueType :db.type/string
-    :db/cardinality :db.cardinality/one
-    :db/fulltext false
-    :db/doc "The sha of the HEAD"
-    :db.install/_attribute :db.part/db}
+   (make-schema-part :github.head/sha
+                     :db.type/string
+                     :db.cardinality/one
+                     "The sha of the HEAD")
    ; user; github.user
-   {:db/id (d/tempid :db.part/db)
-    :db/ident :github.head/user
-    :db/valueType :db.type/ref ; github.user
-    :db/cardinality :db.cardinality/one
-    :db/fulltext false
-    :db/doc "The user that created the commit"
-    :db.install/_attribute :db.part/db}
+   (make-schema-part :github.head/user
+                     :db.type/ref       ; github.user
+                     :db.cardinality/one
+                     "The user that created the commit")
    ; repo
-   {:db/id (d/tempid :db.part/db)
-    :db/ident :github.head/repository
-    :db/valueType :db.type/ref ; github.repository
-    :db/cardinality :db.cardinality/one
-    :db/fulltext false
-    :db/doc "THe repository to which the HEAD belongs"
-    :db.install/_attribute :db.part/db
-    }
+   (make-schema-part :github.head/repository
+                     :db.type/ref       ; github.repository
+                     :db.cardinality/one
+                     "THe repository to which the HEAD belongs" 
+                      )
 
    ;;; pull request states
    [:db/add (d/tempid :db.part/user) :db/ident :github.pullrequest/open]
    [:db/add (d/tempid :db.part/user) :db/ident :github.pullrequest/closed]
 
-
    ;; pull requests
    ; github.general/id
    ; state ; [open closed]
-   {:db/id (d/tempid :db.part/db)
-    :db/ident :github.pullrequest/state
-    :db/valueType :db.type/ref ; github.pullrequest/{open,closed}
-    :db/cardinality :db.cardinality/one
-    :db/fulltext false
-    :db/doc "The state of the pull request"
-    :db.install/_attribute :db.part/db}
+   (make-schema-part :github.pullrequest/state
+                     :db.type/ref   ; github.pullrequest/{open,closed}
+                     :db.cardinality/one
+                     "The state of the pull request")
    ; merge_commit_sha ; string
-   {:db/id (d/tempid :db.part/db)
-    :db/ident :github.pullrequest/merge-sha
-    :db/valueType :db.type/string
-    :db/cardinality :db.cardinality/one
-    :db/fulltext false
-    :db/doc "The sha of the merge commit"
-    :db.install/_attribute :db.part/db}
+   (make-schema-part :github.pullrequest/merge-sha
+                     :db.type/string
+                     :db.cardinality/one
+                     "The sha of the merge commit")
    ; html_url; string
-   {:db/id (d/tempid :db.part/db)
-    :db/ident :github.pullrequest/html-url
-    :db/valueType :db.type/string
-    :db/cardinality :db.cardinality/one
-    :db/fulltext false
-    :db/doc "The html URL of the pull request"
-    :db.install/_attribute :db.part/db}
+   (make-schema-part :github.pullrequest/html-url
+                     :db.type/string
+                     :db.cardinality/one
+                     "The html URL of the pull request")
    ; number; long
-   {:db/id (d/tempid :db.part/db)
-    :db/ident :github.pullrequest/number
-    :db/valueType :db.type/long
-    :db/cardinality :db.cardinality/one
-    :db/fulltext false
-    :db/doc "The numeric ID of the pull request"
-    :db.install/_attribute :db.part/db}
+   (make-schema-part :github.pullrequest/number
+                     :db.type/long
+                     :db.cardinality/one
+                     "The numeric ID of the pull request")
    ; user; ref
-   {:db/id (d/tempid :db.part/db)
-    :db/ident :github.pullrequest/user
-    :db/valueType :db.type/ref
-    :db/cardinality :db.cardinality/one
-    :db/fulltext false
-    :db/doc "The user ... ?"
-    :db.install/_attribute :db.part/db}
+   (make-schema-part :github.pullrequest/user
+                     :db.type/ref
+                     :db.cardinality/one
+                     "The user ... ?")
    ; base; ref
-   {:db/id (d/tempid :db.part/db)
-    :db/ident :github.pullrequest/base
-    :db/valueType :db.type/ref ; guthub.repp
-    :db/cardinality :db.cardinality/one
-    :db/fulltext false
-    :db/doc "The base repositorty against which the pull request was made"
-    :db.install/_attribute :db.part/db}
+   (make-schema-part :github.pullrequest/base
+                     :db.type/ref       ; guthub.repp
+                     :db.cardinality/one
+                     "The base repositorty against which the pull request was made")
    ; head; ref
+   (make-schema-part :github.pullrequest/head
+                     :db.type/ref
+                     :db.cardinality/one
+                     "The HEAD of the pull request")
+
+
+   ;; builds
    {:db/id (d/tempid :db.part/db)
-    :db/ident :github.pullrequest/head
-    :db/valueType :db.type/ref
-    :db/cardinality :db.cardinality/one
-    :db/fulltext false
-    :db/doc "The HEAD of the pull request"
-    :db.install/_attribute :db.part/db}
+    :db/ident :jenkins.build/number
+    :db/valueType :db.type/long}
+   {:db/id (d/tempid :db.part/db)
+    :db/ident :jenkins.build/url
+    :db/valueType :db.type/string}
    ])
 
 
-(defn make-github-user-tx [github-id users]
-  (fn [coll user-name]
-    (let [user (users user-name)
-          user-id (:id user)
-          token (:token user)]
+;; (defn make-github-user-tx [github-id users]
+;;   (fn [coll user-name]
+;;     (let [user (users user-name)
+;;           user-id (:id user)
+;;           token (:token user)]
+;;       (conj coll {:db/id user-id
+;;                   :github/access-token token
+;;                   :user/username user-name
+;;                   :user/service github-id}))))
+
+
+(defn make-github-user-tx [github-id github-user-ids github-users]
+  (fn [coll user-key]
+    (let [user-id (github-user-ids user-key)
+          user (github-users user-key)]
       (conj coll {:db/id user-id
-                  :github/access-token token
-                  :user/username user-name
+                  :github/access-token (:token user)
+                  :user/username (:name user)
                   :user/service github-id}))))
 
 
@@ -267,17 +235,20 @@
     {:db/id (d/tempid :db.part/user)
      :project/owner (:owner project)
      :project/name (:project project)
-     :project/user (:id (users (:user project)))}))
+     :project/user (users (:user project))}))
 
 
-(defn bootstrap-contents [users projects]
+(defn allocate-user-db-ids [users]
+  (reduce #(assoc %1 %2 (d/tempid :db.part/user)) {} (keys users)))
+
+
+(defn bootstrap-contents [github-users projects]
   (let [github-id (d/tempid :db.part/user)
         jenkins-id (d/tempid :db.part/user)
-        user-ids-tokens (reduce #(assoc %1 %2 {:id (d/tempid :db.part/user) :token (users %2)})
-                                {} (keys users))
-        github-user-txs (reduce (make-github-user-tx github-id user-ids-tokens)
-                                [] (keys user-ids-tokens))
-        project-txs (map (make-project-tx user-ids-tokens) projects)
+        github-user-ids (allocate-user-db-ids github-users)
+        github-user-txs (reduce (make-github-user-tx github-id github-user-ids github-users)
+                                [] (keys github-user-ids))
+        project-txs (map (make-project-tx github-user-ids) projects)
         ]
     (concat github-user-txs
             project-txs
--- test/octobuilder/test/database.clj
+++ test/octobuilder/test/database.clj
@@ -15,6 +15,30 @@
      value)))
 
 
+(deftest test-create-user-tx
+  (let [users {:user1 {:name "name" :token "token"}
+               :user2 {:name "user" :token "foo"}}
+        github-user-ids {:user1 1 :user2 2}]
+    (testing "allocate-user-db-ids"
+      (binding [*state* (ref 0)]
+        (stubbing [d/tempid tempid-stub
+                   ;; fixme: Looks ... wrong
+                   keys [:user1 :user2]]
+                  (is (= (allocate-user-db-ids users) github-user-ids)))))
+    (testing "make-github-user-tx"
+      (let [github-id 0]
+        (is (= (sort-by :db/id (reduce (make-github-user-tx github-id github-user-ids users)
+                                       [] (keys users)))
+               [{:db/id 1
+                 :github/access-token "token"
+                 :user/username "name"
+                 :user/service 0}
+                {:db/id 2
+                 :github/access-token "foo"
+                 :user/username "user"
+                 :user/service 0}]))))))
+
+
 (deftest test-bootstrap-contents
   (let [minimum-output [{:db/id 1
                          :service/name "github"}
@@ -27,32 +51,30 @@
         project-part {:db/id 4
                       :project/owner "me!"
                       :project/name "project"
-                      :project/user "him"}
+                      :project/user 3}
         one-user-output (cons user-part minimum-output)
         one-project-output (concat [user-part project-part] minimum-output)]
-    (let [state (ref 0)]
-      (binding [*state* state]
-        (stubbing [d/tempid tempid-stub]
-                  (let [users {}
-                        projects []]
-                    (testing "no users or projects"
-                      (is (= (bootstrap-contents users projects)
-                             minimum-output)))))))
-    (let [state (ref 0)]
-      (binding [*state* state]
-        (stubbing [d/tempid tempid-stub]
-                  (let [users {"name" "token"}
-                        projects []]
-                    (testing "one user, no projects"
-                      (is (= (bootstrap-contents users projects)
-                             one-user-output)))))))
-    (let [state (ref 0)]
-      (binding [*state* state]
-        (stubbing [d/tempid tempid-stub]
-                  (let [users {"name" "token"}
-                        projects [{:owner "me!"
-                                   :project "project"
-                                   :user "him"}]]
-                    (testing "one user, no projects"
-                      (is (= (bootstrap-contents users projects)
-                             one-user-output)))))))))
+    (binding [*state* (ref 0)]
+      (stubbing [d/tempid tempid-stub]
+                (let [users {}
+                      projects []]
+                  (testing "no users or projects"
+                    (is (= (bootstrap-contents users projects)
+                           minimum-output))))))
+    (binding [*state* (ref 0)]
+      (stubbing [d/tempid tempid-stub]
+                (let [users {:user {:name "name" :token "token"}}
+                      projects []]
+                  (testing "one user, no projects"
+                    (is (= (bootstrap-contents users projects)
+                           one-user-output))))))
+    (binding [*state* (ref 0)]
+      (stubbing [d/tempid tempid-stub]
+                (let [users {:user {:name "name" :token "token"}}
+                      projects [{:owner "me!"
+                                 :project "project"
+                                 :user :user
+                                 :jenkins-job nil}]]
+                  (testing "one user, one project"
+                    (is (= (bootstrap-contents users projects)
+                           one-project-output))))))))
