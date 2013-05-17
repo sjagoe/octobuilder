6e44316833ebb9bce13d01b54d3822ea21a6079c
Simon Jagoe <sjagoe@enthought.com>
2013-05-17 17:37:49 +0100

    Updated secrets.example.clj to reflect new structure

--- src/octobuilder/secrets.example.clj
+++ src/octobuilder/secrets.example.clj
@@ -7,9 +7,16 @@
 (def client-secret "0000000000000000000000000000000000000000")
 
 
-(def users {"username" "user-token"})
+(def jenkins-users {:jenkins-user {:name "jenkins-user"
+                                   :password "jenkins-password"}})
+(def jenkins-jobs {:jenkins-job {:name "Job-Name"
+                                 :location "https://jenkins.host.invalid/job/Job-Name/"
+                                 :user :jenkins-user}})
 
 
-(def projects [{:owner "company"
-                :project "project"
-                :user "username"}])
+(def github-users {:github-user {:name "github-user-name"
+                                 :token "github-token"}})
+(def projects [{:owner "owner"
+                :project "Project"
+                :user :github-user
+                :jenkins-job :jenkins-job}])
